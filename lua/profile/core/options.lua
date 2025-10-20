4a93c74566eb62a90f7819d00dd5c75c68845f8a
collinsbartholomew <collinsomega177@gmail.com>
2025-10-20 15:37:03 +0000

    core: update options with enhanced settings

--- lua/profile/core/options.lua
+++ lua/profile/core/options.lua
@@ -1,43 +1,52 @@
 -- Neovim options
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.termguicolors = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 300
-vim.opt.timeoutlen = 500
+local opt = vim.opt
 
--- Additional UI options
-vim.opt.cursorline = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
-vim.opt.fillchars = {
-  vert = '│',
-  fold = '·',
-  eob = ' ',  -- suppress ~ at EndOfBuffer
-  diff = '⣿', -- alternatives = ⣿ ░ ─ ╱
-  msgsep = '‾',
-  foldopen = '▾',
-  foldsep = '│',
-  foldclose = '▸',
-}
-vim.opt.pumblend = 0  -- Make popup menus fully transparent
-vim.opt.winblend = 0  -- Make floating windows fully transparent
-vim.opt.laststatus = 3  -- Global statusline
+-- General settings
+opt.backup = false
+opt.clipboard = "unnamedplus"
+opt.cmdheight = 1
+opt.completeopt = { "menuone", "noselect" }
+opt.conceallevel = 0
+opt.fileencoding = "utf-8"
+opt.hlsearch = true
+opt.ignorecase = true
+opt.mouse = "a"
+opt.pumheight = 10
+opt.showmode = false
+opt.showtabline = 2
+opt.smartcase = true
+opt.smartindent = true
+opt.splitbelow = true
+opt.splitright = true
+opt.swapfile = false
+opt.termguicolors = true
+opt.timeoutlen = 300
+opt.undofile = true
+opt.updatetime = 300
+opt.writebackup = false
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 4
+opt.signcolumn = "yes"
+opt.wrap = false
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.shortmess:append("c")
+opt.foldcolumn = "1"
+opt.foldlevel = 99
+opt.foldmethod = "expr"
+opt.foldexpr = "v:lua.require'nvim-treesitter.foldexpr'()"
+opt.foldenable = true
+opt.fillchars = { foldopen = "", foldclose = "", fold = " ", foldsep = " ", diff = "", eob = " " }
+opt.laststatus = 3
+opt.winbar = "%=%m %f"
 
--- Enable mouse support
-vim.opt.mouse = 'a'
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- Better search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-
--- Indent settings
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.smartindent = true
+-- Performance
+vim.loader.enable()
